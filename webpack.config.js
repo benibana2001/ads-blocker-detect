d0f16041bf02775146a5da3729a185f84d037150
benibana2001 <benibana2001@gmail.com>
2020-06-02 11:58:30 +0900

    modify npm script

---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  entry: './src/index.ts',
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /.(ts|tsx)$/,
+        loader: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        include: [path.resolve(__dirname, 'src'), /node_modules/],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'file?name=fonts/[name].[ext]',
+      },
+      {
+        test: /\.(png|jpg)$/,
+        include: [path.resolve(__dirname, 'src')],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              // limit: 20000,
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+        },
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true,
+    },
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}
