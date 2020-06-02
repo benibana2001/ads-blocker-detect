f3a60a6c6c6b3a0789e505e37284327c648ef1f4
benibana2001 <benibana2001@gmail.com>
2020-06-02 15:28:29 +0900

    implement not to use webpack and typescript

--- src/ads.ts
+++
@@ -1,8 +0,0 @@
-const createDom = (domId: string) => {
-  const e = document.createElement("div");
-  e.id = domId;
-  e.style.display = "none";
-  document.body.appendChild(e);
-};
-
-export default createDom;
